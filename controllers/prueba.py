6bc3f2516c3347b5493818d4d7a6e7e0e1d82523
Mariano Reingart <reingart@gmail.com>
2016-06-15 19:54:08 -0300

    Agrego controlador de prueba

---
+++ controllers/prueba.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def index():
+    return dict(message="pagina predeterminada")
+
+def abm_ejemplo():
+    return dict(message="ABM EJEMPLO")
+
+def reporte_ejemplo():
+    return dict(message="REPORTE ejemplo")
+
+def registrar_comprobante():
+    return dict(message="REGISTRAR COMPROBANTE")
