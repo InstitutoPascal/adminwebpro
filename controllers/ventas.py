898dfff207e00d398d40e7e03bb21b424579299a
Jorge Benitez <jorgedbenitezd@gmail.com>
2016-06-16 19:12:47 -0300

    agrego estructura basica para controladores del modulo ventas

---
+++ controllers/ventas.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def index(): return dict(message="hello from ventas.py")
+
+
+def abm_clientes():
+    return dict(message="abm clientes")
+
+
+def abm_ventas():
+    return dict(message="abm ventas")
+
+
+def comprobantes():
+    return dict(message="comprobante")
