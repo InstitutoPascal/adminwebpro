707bdf3ab8bfb5e694ca46e064e97fe554d602a1
Nestor Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-06-22 19:01:28 -0300

    cambio nombre a pagos

---
+++ controllers/pagos.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# try something like
+def index(): 
+    return dict(message="hello from ordenpagos.py")
+
+def autorizar_pagos(): 
+    return dict(message="hello from ordenpagos.py")
+
+def abm_chueques(): 
+    return dict(message="hello from ordenpagos.py")
+
+def generar_orden_pagos(): 
+    return dict(message="hello from ordenpagos.py")
+
+def reporte_pagos():
+    return dict(message="hello from ordenpagos.py")
