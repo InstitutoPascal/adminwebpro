894f5e1347c179f0b06ca00a4bafa415d882719d
Nestor Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-06-23 18:19:18 -0300

    tablas comunes

---
+++ models/db_app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Archivo para definir las tablas comunes para todos
+
+db.define_table("cliente",
+    )
+
+db.define_table("proveedores",
+    )
+
+db.define_table("productos",
+    )
