89c6c98fe726351376527aaa780d25d20589366f
Jorge Benitez <jorgedbenitezd@gmail.com>
2016-06-23 19:39:54 -0300

    Agrego tabla clientes con validaciones

--- models/db_app.py
+++ models/db_app.py
@@ -15,6 +15,14 @@ db.define_table("cliente",
     Field("telefono","integer"),
     Field("email","string"),
 )
+db.cliente.condicion_frente_al_iva.requires=IS_IN_SET(["Responsable Inscripto","Monotributista","Consumidor Final"])
+db.cliente.cuit.requires=IS_NOT_IN_DB(db, "cliente.cuit")
+db.cliente.dni.requires=IS_NOT_IN_DB(db, "cliente.dni")
+db.cliente.telefono.requires=IS_NOT_EMPTY()
+db.cliente.direccion.requires=IS_NOT_EMPTY()
+db.cliente.numero.requires=IS_NOT_EMPTY()
+db.cliente.localidad.requires=IS_NOT_EMPTY()
+
 
 db.define_table("proveedor",
       Field("id"),
