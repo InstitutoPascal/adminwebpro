519f2a331c6f9dc8cf0fee4ac56ba46fc9a534dc
Nestor Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-06-30 19:19:45 -0300

    Modelo Pago

---
+++ models/db_pagos.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+db.define_table("pago",
+    Field("id_pagos","id"),
+    Field("num_orden_pago","integer"),
+    Field("fecha","date"),
+    Field("importe","integer"),
+)
+
+db.define_table("cheque",
+    Field("id_cheques","id"),
+    Field("num_cheque","integer"),
+    Field("emision","date"),
+    Field("vencimiento","date"),
+    Field("importe","integer"),
+)
+db.pagos.num_orden_pago.requires=IS_NOT_IN_DB(db, "pagos.num_orden_pago")
+db.pagos.fecha.requires=IS_DATE('%Y-%m-%d')
+db.cheques.num_cheques.requires=IS_NOT_IN_DB(db, "cheques.num_cheques")
+db.cheques.emision.requires=IS_DATE('%Y-%m-%d')
+db.cheques.vencimiento.requires=IS_DATE('%Y-%m-%d')
