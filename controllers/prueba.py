3c1e4663aeb9dee1018fae5c1bd287a66a0dc51e
Mariano Reingart <reingart@gmail.com>
2016-06-30 18:41:57 -0300

    ABM de ejemplo

--- controllers/prueba.py
+++ controllers/prueba.py
@@ -3,6 +3,11 @@
 def index():
     return dict(message="pagina predeterminada")
 
+@auth.requires_login()
+def abm_categoria():
+    grid = SQLFORM.grid(db.categoria)
+    return {"grilla": grid}
+
 def abm_ejemplo():
     return dict(message="ABM EJEMPLO")
 
