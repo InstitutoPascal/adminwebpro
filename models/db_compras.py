dc4833c2492b9f4ac7b9d58af31b88f4c8533861
pablosegovia <none@none>
2016-07-02 16:59:54 -0300

    erge

--- models/db_compras.py
+++ models/db_compras.py
@@ -8,26 +8,26 @@ db.define_table("factura_compra",
       Field("razon_social", 'string'),
       Field("tipo_iva", 'float'),
       Field("ingresos_brutos", 'integer'),
-      Field("ptipo_insumo", 'string'),
+      Field("tipo_insumo", 'string'),
       Field("domicilio", 'string'),
       Field("localidad", 'string'),
       Field("codigo_postal", 'integer'),
       Field("provincia", 'string'),
       Field("telefono", 'integer'),
     )
-db.compra.tipo_factura.requires=IS_IN_SET(["Factua A","Factura B"])
-db.compra.tipo_iva.requires=IS_IN_SET({27:"Servicios 27%", 21:"Tasa General 21%", 10.5:"Tasa Reducida 10,5%"})
-db.compra.tipo_insumo.requires=IS_IN_SET(["Servicio","Producto"])
-db.compra.numero_factura.requires=IS_NOT_EMPTY()
-db.compra.cuit.requires=IS_NOT_EMPTY()
-db.compra.tipo_factura.requires=IS_NOT_EMPTY()
-db.compra.fecha_factura.requires=IS_NOT_EMPTY()
-db.compra.razon_social.requires=IS_NOT_EMPTY()
-db.compra.tipo_iva.requires=IS_NOT_EMPTY()
-db.compra.ingresos_brutos.requires=IS_NOT_EMPTY()
-db.compra.tipo_insumo.requires=IS_NOT_EMPTY()
-db.compra.domicilio.requires=IS_NOT_EMPTY()
-db.compra.localidad.requires=IS_NOT_EMPTY()
-db.compra.codigo_postal.requires=IS_NOT_EMPTY()
-db.compra.provinia.requires=IS_NOT_EMPTY()
-db.compra.telefono.requires=IS_NOT_EMPTY()
+db.factura_compra.tipo_factura.requires=IS_IN_SET(["Factua A","Factura B"])
+db.factura_compra.tipo_iva.requires=IS_IN_SET({27:"Servicios 27%", 21:"Tasa General 21%", 10.5:"Tasa Reducida 10,5%"})
+db.factura_compra.tipo_insumo.requires=IS_IN_SET(["Servicio","Producto"])
+db.factura_compra.numero_factura.requires=IS_NOT_EMPTY()
+db.factura_compra.cuit.requires=IS_NOT_EMPTY()
+db.factura_compra.tipo_factura.requires=IS_NOT_EMPTY()
+db.factura_compra.fecha_factura.requires=IS_NOT_EMPTY()
+db.factura_compra.razon_social.requires=IS_NOT_EMPTY()
+db.factura_compra.tipo_iva.requires=IS_NOT_EMPTY()
+db.factura_compra.ingresos_brutos.requires=IS_NOT_EMPTY()
+db.factura_compra.tipo_insumo.requires=IS_NOT_EMPTY()
+db.factura_compra.domicilio.requires=IS_NOT_EMPTY()
+db.factura_compra.localidad.requires=IS_NOT_EMPTY()
+db.factura_compra.codigo_postal.requires=IS_NOT_EMPTY()
+db.factura_compra.provincia.requires=IS_NOT_EMPTY()
+db.factura_compra.telefono.requires=IS_NOT_EMPTY()
