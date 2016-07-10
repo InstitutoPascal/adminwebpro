9e7be1b4a291cc0560a90cee552a818b43335700
Jorge Benitez <jorgedbenitezd@gmail.com>
2016-07-10 11:17:16 -0300

    Subo codigo del modulo ventas

--- models/db_ventas.py
+++ models/db_ventas.py
@@ -12,10 +12,10 @@ db.define_table("ventas",
     Field("iva","double"),
     Field("descuento","double"),
     )
-db.ventas.buscar_cliente.requires = IS_IN_DB(db, "cliente.id_cliente", "-- %(nombre_de_fantasia)s %(razon_social)s ..")
+db.ventas.buscar_cliente.requires = IS_IN_DB(db, "cliente.id_cliente", " %(nombre_de_fantasia)s %(razon_social)s ..")
 db.ventas.tipo_de_factura.requires=IS_IN_SET(["Factura A","Factura B"])
 db.ventas.tipo_de_pago.requires = IS_IN_SET(["Efectivo","Credito"])
 db.ventas.tipo_de_producto.requires = IS_IN_SET(["Hardware","Software"])
-#db.ventas.detalle.requires=
+db.ventas.detalle.requires=IS_IN_DB(db, "producto.id_producto", " %(detalle_producto)s")
 db.ventas.iva.requires = IS_IN_SET(["10.5%","21%","27%"])
 db.ventas.descuento.requires = IS_IN_SET(["10%","15%","25%","40%"])
