d48fc54cf862aa678fae22e6109c42c8e2b85211
Francisco Rodriguez <franciscosrc03@gmail.com>
2016-07-14 20:08:30 -0300

    se agrego vista de comprobantes

--- models/db_compras.py
+++ models/db_compras.py
@@ -14,23 +14,23 @@ db.define_table("compra",
       Field("provincia", 'string'),
       Field("telefono", 'integer'),
     )
-db.factura_compra.tipo_factura.requires=IS_IN_SET(["Factua A","Factura B"])
-db.factura_compra.tipo_iva.requires=IS_IN_SET({27:"Servicios 27%", 21:"Tasa General 21%", 10.5:"Tasa Reducida 10,5%"})
-db.factura_compra.numero_factura.requires=IS_NOT_EMPTY()
-db.factura_compra.cuit.requires=IS_NOT_EMPTY()
-db.factura_compra.fecha_factura.requires=IS_NOT_EMPTY()
-db.factura_compra.razon_social.requires=IS_NOT_EMPTY()
-db.factura_compra.ingresos_brutos.requires=IS_NOT_EMPTY()
-db.factura_compra.domicilio.requires=IS_NOT_EMPTY()
-db.factura_compra.localidad.requires=IS_NOT_EMPTY()
-db.factura_compra.codigo_postal.requires=IS_NOT_EMPTY()
-db.factura_compra.provincia.requires=IS_NOT_EMPTY()
-db.factura_compra.telefono.requires=IS_NOT_EMPTY()
+db.compra.tipo_factura.requires=IS_IN_SET(["Factua A","Factura B"])
+db.compra.tipo_iva.requires=IS_IN_SET({27:"Servicios 27%", 21:"Tasa General 21%", 10.5:"Tasa Reducida 10,5%"})
+db.compra.numero_factura.requires=IS_NOT_EMPTY()
+db.compra.cuit.requires=IS_NOT_EMPTY()
+db.compra.fecha_factura.requires=IS_NOT_EMPTY()
+db.compra.razon_social.requires=IS_NOT_EMPTY()
+db.compra.ingresos_brutos.requires=IS_NOT_EMPTY()
+db.compra.domicilio.requires=IS_NOT_EMPTY()
+db.compra.localidad.requires=IS_NOT_EMPTY()
+db.compra.codigo_postal.requires=IS_NOT_EMPTY()
+db.compra.provincia.requires=IS_NOT_EMPTY()
+db.compra.telefono.requires=IS_NOT_EMPTY()
 
 db.define_table("detalle_compra",
       Field("id_producto", db.producto),
       Field("id_compra", db.compra),
-      Field("id",db.probeedor),
+      Field("id",db.proveedor),
       Field("precio", 'float'),
       Field("cantidad", 'integer'),
                 )
