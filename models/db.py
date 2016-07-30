45ae5de0ae087f9c7f6ed0d07bef88bf0177dd09
pablo <pablo.segovia79@gmail.com>
2016-07-30 11:45:26 -0300

    erge

--- models/db.py
+++ models/db.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# -------------------------------------------------------------------------
-# This scaffolding model makes your app work on Google App Engine too
-# File is released under public domain and you can use without limitations
-# -------------------------------------------------------------------------
-
 if request.global_settings.web2py_version < "2.14.1":
     raise HTTP(500, "Requires web2py 2.13.3 or newer")
 
