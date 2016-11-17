4b8c55870b6f4e813ea62266f5d13aaab1f26125
Leonel Daruich <leodaruich2@gmail.com>
2016-11-16 23:43:33 -0300

    se agregan validaciones al formulario #206

--- controllers/sueldos.py
+++ controllers/sueldos.py
@@ -23,67 +23,76 @@ def abm_familiares():
 def abm_horas():
     grid = SQLFORM.grid(db.horas)
     return {"grilla": grid}
-
 def legajos():
+    import os
+    form = SQLFORM.factory(Field('nro_legajo',requires=IS_NOT_EMPTY(error_message='Ingrese el número de legajo')),
+                           Field('fecha_egreso','date',requires=IS_NOT_EMPTY(error_message='Ingrese la fecha')),
+                          Field('cuil',requires=IS_NOT_EMPTY(error_message='Ingrese el cuil')),
+                           Field('dni',requires=IS_NOT_EMPTY(error_message='Ingrese el dni')),
+                           Field('corresponde_hs_extra',requires=IS_IN_SET({1:'si',2:'no'},error_message='Ingrese una opción',zero='Seleccionar...')),
+                           Field('nombres',requires=IS_NOT_EMPTY(error_message='Ingrese el nombre')),
+                           Field('apellido',requires=IS_NOT_EMPTY(error_message='Ingrese el apellido')),
+                           Field('fecha_nacimiento','date'),
+                           Field('lugar_nacimiento','string'),
+                           Field('estado_civil','string'),
+                           Field('edad','string'),
+                           Field('categoria','string'),
+                           Field('domicilio_calle','string'),
+                           Field('numero_calle','string'),
+                           Field('piso','string'),
+                           Field('depto','string'),
+                           Field('imagen','upload',uploadfolder=os.path.join(request.folder,'uploads')),
+                           submit_button='Siguiente'
+                          )
+
+    if form.process().accepted:
+        try:
+            image = db.legajos.image.store(request.vars["imagen"].file, request.vars["imagen"].filename)
+            session["imagen"] = image
+        except:
+            session['imagen'] = None
+        session["nro_legajo"] = request.vars['nro_legajo']
+        session["fecha_egreso"] = request.vars['fecha_egreso']
+        session["cuil"] = request.vars['cuil']
+        session["dni"] = request.vars['dni']
+        session["horas_extras"] = request.vars['corresponde_hs_extra']
+        session["nombre"] = request.vars['nombres']
+        session["apellido"] = request.vars['apellido']
+        session["fecha_nacimiento"] = request.vars['fecha_nacimiento']
+        session["lugar_nacimiento"] = request.vars['lugar_nacimiento']
+        session["estado_civil"] = request.vars['estado_civil']
+        session["edad"] = request.vars['edad']
+        session["categoria"] = request.vars['categoria']
+        session["domicilio"] = request.vars['domicilio_calle']
+        session["num_domicilio"] = request.vars['numero_calle']
+        session["piso"] = request.vars['piso']
+        session["depto"] = request.vars['depto']
+        redirect(URL(c='sueldos',f='legajos2'))
+    
     grid = SQLFORM.grid(db.legajos)
-    return {"grilla": grid}
-
-def legajos():
-    grid = SQLFORM.grid(db.legajos)
-    return {"grilla": grid}
-
-
+    return locals()
 
 def legajos2():
-     if request.vars["buton_siguiente"]:
-        # obtengo los valores completados en el formulario
-        image = db.legajos.image.store(request.vars["imagen"].file, request.vars["imagen"].filename)
-        nro_legajo = request.vars["nro_legajo"]
-        fecha_egreso = request.vars["fecha_egreso"]
-        cuil = request.vars["cuil"]
-        dni = request.vars["dni"]
-        horas_extras = request.vars["horas_extras"]
-        nombre = request.vars["nombre"]
-        apellido = request.vars["apellido"]
-        fecha_nacimiento = request.vars["fecha_nacimiento"]
-        lugar_nacimiento = request.vars["lugar_nacimiento"]
-        estado_civil = request.vars["estado_civil"]
-        edad = request.vars["edad"]
-        categoria = request.vars["categoria"]
-        domicilio = request.vars["domicilio"]
-        num_domicilio = request.vars["num_domicilio"]
-        piso = request.vars["piso"]
-        depto = request.vars["depto"]
-
-        # guardo los datos elegidos en la sesión
-        session["imagen"] = image
-        session["nro_legajo"] = nro_legajo
-        session["fecha_egreso"] = fecha_egreso
-        session["cuil"] = cuil
-        session["dni"] = dni
-        session["horas_extras"] = horas_extras
-        session["nombre"] = nombre
-        session["apellido"] = apellido
-        session ["fecha_nacimiento"] = fecha_nacimiento
-        session ["lugar_nacimiento"] = lugar_nacimiento
-        session ["estado_civil"] = estado_civil
-        session ["edad"] = edad
-        session ["categoria"] = categoria
-        session ["domicilio"] = domicilio
-        session ["num_domicilio"] = num_domicilio
-        session ["piso"] = piso
-        session ["depto"] = depto
-        grid = SQLFORM.grid(db.legajos)
-        return {"grilla": grid}
-
-def legajos2():
-    grid = SQLFORM.grid(db.legajos)
-    return {"grilla": grid}
-
-def legajos3():
-    print session ["imagen"]
-    id = db.legajos.insert(
-            image = session ["imagen"],
+    form = SQLFORM.factory(Field('obra_social'),
+                           Field('codigo_postal'),
+                           Field('localidad'),
+                           Field('email'),
+                           Field('fecha_ingreso','date'),
+                           Field('telefono',label='Teléfono'),
+                           Field('telefono_celular',label='Teléfono Celular'),
+                           Field('estudia',label='Estudia?',requires=IS_IN_SET(['si','no'],zero='Seleccionar...',error_message='Indique una opción')),
+                           Field('cons_cuil',label='Constancia de cuil',requires=IS_IN_SET(['si','no'],zero='Seleccionar...',error_message='Indique una opción')),
+                           Field('alt_tem',label='Alta temprana',requires=IS_IN_SET(['si','no'],zero='Seleccionar...',error_message='Indique una opción')),
+                           Field('foto_dni',label='Fotocopia de DNI',requires=IS_IN_SET(['si','no'],zero='Seleccionar...',error_message='Indique una opción')),
+                           Field('libreta',requires=IS_IN_SET(['corresponde','no corresponde'],zero='Seleccionar...',error_message='Indique una opción')),
+                           Field('lib_pre','boolean'),
+                           Field('partida',requires=IS_IN_SET(['corresponde','no corresponde'],zero='Seleccionar...',error_message='Indique una opción')),
+                           Field('part_pre','boolean'),
+                          )
+    if form.process().accepted:
+        print session['depto']
+        id = db.legajos.insert(
+            image = session["imagen"],
             num_legajo = session["nro_legajo"],
             fecha_egreso = session["fecha_egreso"],
             cuil = session["cuil"],
@@ -91,19 +100,26 @@ def legajos3():
             horas_extras = session["horas_extras"],
             nombre = session["nombre"],
             apellido = session["apellido"],
-            fe_nac = session ["fecha_nacimiento"] ,
-            lu_nac = session ["lugar_nacimiento"],
-            est_civ = session ["estado_civil"],
-            edad = session ["edad"],
-            categoria = session ["categoria"],
-            dom_calle = session ["domicilio"],
-            dom_numero= session ["num_domicilio"],
-            piso = session ["piso"],
-            depto = session ["depto"],
+            fe_nac = session["fecha_nacimiento"] ,
+            lu_nac = session["lugar_nacimiento"],
+            est_civ = session["estado_civil"],
+            edad = session["edad"],
+            categoria = session["categoria"],
+            dom_calle = session["domicilio"],
+            dom_numero= session["num_domicilio"],
+            piso = session["piso"],
+            depto = session['depto']
+        )
+        #EN el insert faltan agregar los datos que estan en este form 
+        
+        redirect(URL(c='sueldos',f='legajos3'))
+    
+    return locals()
 
-    )
+def legajos3():
+    print session ["imagen"]
+    
     return {"msg": "se agrego id = %s" % id}
-
 
 
 def horas():
@@ -130,13 +146,13 @@ def reportes_empleados():
     dt_obj2 = datetime.strptime(dt_str2, '%Y-%m-%d')
     fecha_hasta = dt_obj2
     ordenar = request.vars["ordenar"]
-
+    
     campos = db.legajos.num_legajo, db.legajos.nombre, db.legajos.apellido, db.legajos.fecha_ingreso
     criterio = db.legajos.fecha_ingreso >= fecha_desde
     criterio &= db.legajos.fecha_ingreso <= fecha_hasta
-
+    
     if ordenar:
-        orden = db.legajos.apellido, db.legajos.nombre,
+        orden = db.legajos.apellido, db.legajos.nombre, 
     else:
         orden = db.legajos.num_legajo
 
@@ -155,13 +171,13 @@ def reportes_horas():
     Legajo = request.vars["Legajo"]
     mes = request.vars["mes"]
     ordenar = request.vars["ordenar"]
-
+    
     campos = db.horas.num_legajo, db.legajos.nombre, db.legajos.apellido, db.horas.hs_trab, db.horas.mes_trabajado
     criterio = db.horas.num_legajo >= Legajo
     criterio &= db.horas.mes_trabajado == mes
-
+    
     if ordenar:
-        orden = db.legajos.apellido, db.legajos.nombre,db.horas.hs_trab, db.horas.mes_trabajado,
+        orden = db.legajos.apellido, db.legajos.nombre,db.horas.hs_trab, db.horas.mes_trabajado, 
     else:
         orden = db.legajos.num_legajo
 
@@ -177,10 +193,10 @@ def reportes_familiares():
     familia_menor21 = request.vars["familia_menor21"]
     familia_estudian = request.vars["familia_estudian"]
     familiar_distdom = request.vars["familiar_distdom"]
-
-    campos = db.familiares.num_legajo, db.familiares.nombre, db.familiares.apellido, db.familiares.estudia, db.familiares.edad, db.familiares.domicilio_calle, db.legajos.dom_calle, db.familiares.domicilio_numero
+    
+    campos = db.familiares.num_legajo, db.familiares.nombre, db.familiares.apellido, db.familiares.estudia, db.familiares.edad, db.familiares.domicilio_calle, db.legajos.dom_calle, db.familiares.domicilio_numero 
     criterio = db.familiares.num_legajo == db.legajos.num_legajo
-
+    
     if familia_menor21:
         criterio &=  db.familiares.edad < 21
         subtitulo = "Familiares menores de 21 años"
@@ -189,7 +205,8 @@ def reportes_familiares():
         subtitulo = "Familiares que estudian"
     if familiar_distdom:
         criterio = db.familiares.num_legajo == db.legajos.num_legajo
-        criterio &=  ((db.familiares.domicilio_calle != db.legajos.dom_calle) |  (db.familiares.domicilio_numero != db.legajos.dom_numero))
+        criterio &=  db.familiares.domicilio_calle != db.legajos.dom_calle
+        criterio |=  db.familiares.domicilio_numero != db.legajos.dom_numero
         subtitulo = "Familiares con distinto domicilio"
     registros = db(criterio).select(*campos)
     return dict(lista_familiares=registros,titulo="Listando %s" % subtitulo)
